7d956a8e088925c29b7dd828cc97598045bbf7e3
JuliaKh <myrashkam8@mail.ru>
2018-11-20 16:53:43 +0200

    update

--- Java script/Noughts and crosses/javascript.js
+++ Java script/Noughts and crosses/javascript.js
@@ -1,147 +1,101 @@
-window.onload = function (){
-       
-  class getElements{
-    constructor(selector){
-       this.el = document.querySelectorAll(selector);
+var playGame = (function() {
+
+  var tern = document.querySelector('.tern');
+  var blocks = document.querySelectorAll('.block');
+  var restartButton = document.querySelector('.restartButton');
+  var wonx = document.querySelector('.wonX');
+  var wono = document.querySelector('.wonO');
+  var draw = document.querySelector('.draw');
+  var checkDraw = countDraw();
+  var check = checkWinner();
+  var counter = 0;
+
+
+  function makeMove() {
+    for (var i =0 ; i < blocks.length; i++) {
+        blocks[i].addEventListener('click', move);
     }
-    on(event,callback){
-      for(var i =0; i < this.el.length; i++){
-        this.el[i].addEventListener(event, callback);
-      }
-      return this;
+  }
+
+
+  restartButton.addEventListener('click', function(){
+    return restart(blocks);
+  });
+
+
+  function move() {
+    if (counter % 2 == 0 && counter < 8) {
+       this.innerHTML = 'X';
+       tern.innerHTML = '0';
+       check('X');
+    } 
+    else if (counter % 2 != 0 && counter < 8) {
+       this.innerHTML = '0';
+       tern.innerHTML = 'X';
+       check('0');
+    } 
+    else if (counter == 8) {
+       checkDraw();
     }
+    counter++;
   }
 
 
-class getElement{
-  constructor(selector){
-    this.el = document.querySelector(selector);
-    return this.el;
-  }
-}
-
-class getElementsSelf{
-  constructor(selector){
-    this.el = document.querySelectorAll(selector);
-     return Array.from(this.el);
-  }
-}
-
-
-//--functions--------------------
-var counter = 0;
-
-function move (){
-  if(counter % 2==0 && counter < 8){
-    this.innerHTML = 'X';
-    tern.innerHTML = '0';
-    check('X');
-  } else if(counter % 2 !=0 && counter < 8){
-    this.innerHTML = '0';
+  function clean(el) {
+    for (var i = 0; i < el.length; i++) {
+        el[i].innerHTML = '';
+    }
     tern.innerHTML = 'X';
-    check('0');
-  } else if(counter ==8){
-    checkDraw();
   }
-  counter++;
-}
 
 
-function restart(el){
-  for(var i =0; i < el.length; i++){
-      el[i].innerHTML = '';
+  function restart(el) {
+    clean(el);
+    counter = 0;
   }
-   tern.innerHTML = 'X';
-   counter = 0;
-}
- 
- function clear (el){
-   for(var i =0; i < el.length; i++){
-      el[i].innerHTML = '';
+   
+
+  function clear(el) {
+    clean(el);
+    counter = -1;
   }
-   tern.innerHTML = 'X';
-   counter = -1;
- }
 
-function checkWinner(){
-   var wonX = 0;
-   var wonO = 0;
-   return function(player){
-      if(blocks[0].innerHTML == player && blocks[1].innerHTML == player && blocks[2].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
+
+  function countDraw() {
+    var countD = 0;
+
+    return function () {
+        alert ('Draw');
+        draw.innerHTML = ++countD;
+        restart(blocks);
+    }
+  }
+
+
+  function checkWinner() {
+    var wonX = 0;
+    var wonO = 0;
+    var winner; 
+    var combinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+    return function(player) {
+
+      for (var i = 0; i < combinations.length; i++) {
+        if (blocks[combinations[i][0]].innerHTML == player && blocks[combinations[i][1]].innerHTML == player && blocks[combinations[i][2]].innerHTML == player) {
+           winner = player;
+           alert ('Won ' + player);
+           clear(blocks);
+
+           if(winner == 'X') return wonx.innerHTML = ++wonX;
+           if(winner == '0') return wono.innerHTML = ++wonO;
+        }
       }
-      if(blocks[3].innerHTML == player && blocks[4].innerHTML == player && blocks[5].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
-      }
-      if(blocks[6].innerHTML == player && blocks[7].innerHTML == player && blocks[8].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
-      }
-      if(blocks[0].innerHTML == player && blocks[3].innerHTML == player && blocks[6].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
-      }
-      if(blocks[1].innerHTML == player && blocks[4].innerHTML == player && blocks[7].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
-      }
-      if(blocks[2].innerHTML == player && blocks[5].innerHTML == player && blocks[8].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
-      }
-      if(blocks[0].innerHTML == player && blocks[4].innerHTML == player && blocks[8].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
-      }
-      if(blocks[2].innerHTML == player && blocks[4].innerHTML == player && blocks[6].innerHTML == player){
-          alert ('Won ' + player);
-          var winner = player;
-          clear(blocks);
-      }
-          if(winner == 'X') return wonx.innerHTML = ++wonX;
-          if(winner == '0') return wono.innerHTML = ++wonO;
-     }
-}
+    }
+  }
 
-var check = checkWinner();
+  return {
+        makeMove:  makeMove
+  }
+})();
 
-function countDraw(){
-   var countD = 0;
-   return function (){
-      alert ('Draw');
-      draw.innerHTML = ++countD;
-      restart(blocks);
-   }
-}
-
-var checkDraw = countDraw();
-
-//Main code -------------------------
-
-var tern = new getElement('.tern');
-
-(new getElements('.block')).on('click', move);
-
-var blocks = new getElementsSelf('.block');
-
-(new getElements('.restart')).on('click', function(){
-  return restart(blocks);
-});
-
-var wonx = new getElement('.wonX');
-var wono = new getElement('.wonO');
-var draw = new getElement('.draw');
-
-
-
-
-}
+playGame.makeMove();
